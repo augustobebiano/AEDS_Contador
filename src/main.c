3b16cb7d7c731eef89143093bddfb83d14988d6b
hookez <59745764+hookez@users.noreply.github.com>
2022-04-05 11:28:38 -0300

    Delete src directory

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "matriz.h"
-
-int main(){
-    int m = 5;
-    matriz(m);
-    return 0;
-}
--- src/matriz.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIZ_H
-#define MATRIZ_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void contador(int m);
-
-#endif
